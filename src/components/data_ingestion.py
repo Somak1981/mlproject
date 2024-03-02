fcb12aebc543e54562ec32227d0df6f5ac0ca991
Somak1981 <somak.banerjee.work@gmail.com>
2024-03-02 10:56:07 +0000

    Data Transformation completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,15 +9,22 @@ from sklearn.model_selection import train_test_split
 
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTranformation
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path:str=os.path.join('artifacts',"train.csv")
     test_data_path:str=os.path.join('artifacts',"test.csv")
     raw_data_path:str=os.path.join('artifacts',"raw.csv")
 
-# 1. Initialize the variables of the class
-# 2. Read the dataset containing the data
-# 3. 
+# -------------------------------------------------------------------------------------------------#
+# 1. Initialize the variables of the class                                                         #
+# 2. Read the dataset containing the data                                                          #
+# 3. Create the directories for train, test and raw                                                #
+# 4. perform train test split using the model selection in sklearn                                 #
+# 5. create the raw, train and test dataset in the location created in step 3                      #
+# -------------------------------------------------------------------------------------------------#
 class DataIngestion:
     def __init__(self) :
         self.ingestion_config=DataIngestionConfig()             #initialize the variables
@@ -52,5 +59,9 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data=obj.initiate_data_ingestion()
+
+    data_transformation = DataTranformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
+
 
